2ff917bb64887ce659b04213c396c26933338efa
caronorato-lab <caronorato@gmail.com>
2026-05-09 10:53:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Logo de la Defensoría del Pueblo (URL pública oficial de Wikipedia)
-st.image("logo_defensoria.png", width=200)
+st.image("logo_defensoria.png", width=500)
 
 
 # Caché de Streamlit: La clave del éxito. 
